12dcf82e52bc205d0f3b0323a2753dce7cbcd254
K R HARI PRAJWAL <hariprajwal77@gmail.com>
2026-02-20 13:27:31 +0530

    Update homepage.jsx

--- homepage.jsx
+++ homepage.jsx
@@ -1,53 +1,21 @@
-import React from "react";
-import "./homepage.css";
-import { FaUserCircle, FaFolderOpen, FaCog, FaSignOutAlt } from "react-icons/fa";
-
-export default function HomePage() {
+export default function HomePage({ user }) {
   return (
-    <div className="home-container">
-      <div className="glass-card">
+    <>
+      <h2>Welcome {user} 👋</h2>
 
-        {/* Sidebar */}
-        <div className="sidebar">
-          <div className="profile">
-            <FaUserCircle size={70} />
-            <p>User</p>
-          </div>
-
-          <div className="menu">
-            <button className="active">Home</button>
-            <button>Vault</button>
-            <button>Settings</button>
-          </div>
-
-          <div className="logout">
-            <button>
-              <FaSignOutAlt /> Logout
-            </button>
-          </div>
-        </div>
-
-        {/* Main Dashboard */}
-        <div className="dashboard">
-          <h2>Welcome User 👋</h2>
-
-          <div className="search-bar">
-            <input type="text" placeholder="Search files..." />
-          </div>
-
-          <div className="quick-actions">
-            <div className="card"></div>
-            <div className="card"></div>
-            <div className="card"></div>
-          </div>
-
-          <div className="vault-section">
-            <h3>Vault</h3>
-            <button className="add-btn">+ Add Personal Info</button>
-          </div>
-        </div>
-
+      <div className="search-bar">
+        <input type="text" placeholder="Search files..." />
       </div>
-    </div>
+
+      <div className="quick-actions">
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+      </div>
+
+      <div className="vault-section">
+        <h3>Vault</h3>
+      </div>
+    </>
   );
 }
